eb0bec3e56072dc87f941336be48e1d53776721a
ss2ss2 <usa.saru.2525@gmail.com>
2026-01-12 10:45:37 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-'use strict'
+'use strict';
 
-const rotateButton = document.getElementById('rotate-btn');
-const backgroundLayer = document.getElementById('background-layer');
-
-rotateButton.addEventListener('click', () => {
-  backgroundLayer.classList.toggle('is-rotated');
-});
+console.log("ポートフォリオサイトの準備完了！");
