41babac389c5bcff421373f730cc058e7a968c23
ss2ss2 <usa.saru.2525@gmail.com>
2026-01-12 10:55:57 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-console.log("ポートフォリオサイトの準備完了！");
+// 要素を取得
+const rotateButton = document.getElementById('rotate-btn');
+const backgroundLayer = document.getElementById('background-layer');
+
+// ボタンクリック時の動作
+rotateButton.addEventListener('click', () => {
+  backgroundLayer.classList.toggle('is-rotated');
+});
